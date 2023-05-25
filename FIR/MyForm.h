7dff112e008ff10b7100277950853b5acb92cf71
AscheRU <128119120+AscheRU@users.noreply.github.com>
2023-05-25 15:36:19 +1000

    progressBar

--- FIR/MyForm.h
+++ FIR/MyForm.h
@@ -97,6 +97,9 @@ namespace FIR {
 	private: System::Windows::Forms::SaveFileDialog^  saveFileDialog1;
 	private: System::Windows::Forms::Button^  button_data_processing;
 	private: System::Windows::Forms::Button^  btn_mini;
+	public: System::Windows::Forms::ProgressBar^  progressBar1;
+	private:
+
 
 
 
@@ -124,12 +127,12 @@ namespace FIR {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^  legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^  legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^  series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea3 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea4 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->btn_exit = (gcnew System::Windows::Forms::Button());
 			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->chart2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
@@ -156,6 +159,7 @@ namespace FIR {
 			this->saveFileDialog1 = (gcnew System::Windows::Forms::SaveFileDialog());
 			this->button_data_processing = (gcnew System::Windows::Forms::Button());
 			this->btn_mini = (gcnew System::Windows::Forms::Button());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->BeginInit();
 			this->SuspendLayout();
@@ -172,51 +176,51 @@ namespace FIR {
 			// 
 			this->chart1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(140)));
-			chartArea1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
+			chartArea3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(140)));
-			chartArea1->Name = L"ChartArea1";
-			this->chart1->ChartAreas->Add(chartArea1);
-			legend1->Alignment = System::Drawing::StringAlignment::Center;
-			legend1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
+			chartArea3->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea3);
+			legend3->Alignment = System::Drawing::StringAlignment::Center;
+			legend3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(140)));
-			legend1->Docking = System::Windows::Forms::DataVisualization::Charting::Docking::Top;
-			legend1->Name = L"Legend1";
-			this->chart1->Legends->Add(legend1);
+			legend3->Docking = System::Windows::Forms::DataVisualization::Charting::Docking::Top;
+			legend3->Name = L"Legend1";
+			this->chart1->Legends->Add(legend3);
 			resources->ApplyResources(this->chart1, L"chart1");
 			this->chart1->Name = L"chart1";
-			series1->BorderWidth = 3;
-			series1->ChartArea = L"ChartArea1";
-			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
-			series1->Color = System::Drawing::Color::White;
-			series1->Legend = L"Legend1";
-			series1->LegendText = L"ИХ";
-			series1->Name = L"Series1";
-			this->chart1->Series->Add(series1);
+			series3->BorderWidth = 3;
+			series3->ChartArea = L"ChartArea1";
+			series3->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
+			series3->Color = System::Drawing::Color::White;
+			series3->Legend = L"Legend1";
+			series3->LegendText = L"ИХ";
+			series3->Name = L"Series1";
+			this->chart1->Series->Add(series3);
 			// 
 			// chart2
 			// 
 			this->chart2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(140)));
-			chartArea2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
+			chartArea4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(140)));
-			chartArea2->Name = L"ChartArea1";
-			this->chart2->ChartAreas->Add(chartArea2);
-			legend2->Alignment = System::Drawing::StringAlignment::Center;
-			legend2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
+			chartArea4->Name = L"ChartArea1";
+			this->chart2->ChartAreas->Add(chartArea4);
+			legend4->Alignment = System::Drawing::StringAlignment::Center;
+			legend4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(140)));
-			legend2->Docking = System::Windows::Forms::DataVisualization::Charting::Docking::Top;
-			legend2->Name = L"Legend1";
-			this->chart2->Legends->Add(legend2);
+			legend4->Docking = System::Windows::Forms::DataVisualization::Charting::Docking::Top;
+			legend4->Name = L"Legend1";
+			this->chart2->Legends->Add(legend4);
 			resources->ApplyResources(this->chart2, L"chart2");
 			this->chart2->Name = L"chart2";
-			series2->BorderWidth = 3;
-			series2->ChartArea = L"ChartArea1";
-			series2->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
-			series2->Color = System::Drawing::Color::White;
-			series2->Legend = L"Legend1";
-			series2->LegendText = L"Спектр ИХ";
-			series2->Name = L"Series1";
-			this->chart2->Series->Add(series2);
+			series4->BorderWidth = 3;
+			series4->ChartArea = L"ChartArea1";
+			series4->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
+			series4->Color = System::Drawing::Color::White;
+			series4->Legend = L"Legend1";
+			series4->LegendText = L"Спектр ИХ";
+			series4->Name = L"Series1";
+			this->chart2->Series->Add(series4);
 			// 
 			// tBox1
 			// 
@@ -355,12 +359,18 @@ namespace FIR {
 			this->btn_mini->UseVisualStyleBackColor = true;
 			this->btn_mini->Click += gcnew System::EventHandler(this, &MyForm::btn_mini_Click);
 			// 
+			// progressBar1
+			// 
+			resources->ApplyResources(this->progressBar1, L"progressBar1");
+			this->progressBar1->Name = L"progressBar1";
+			// 
 			// MyForm
 			// 
 			resources->ApplyResources(this, L"$this");
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(140)));
+			this->Controls->Add(this->progressBar1);
 			this->Controls->Add(this->btn_mini);
 			this->Controls->Add(this->button_data_processing);
 			this->Controls->Add(this->button_saveF);
@@ -718,19 +728,27 @@ namespace FIR {
 			//КНОПКА обработки файла
 	private: System::Void button_data_processing_Click(System::Object^  sender, System::EventArgs^  e) {
 		this->labelStat->Text = "Идет обработка данных";
-		MessageBox::Show("Для начала обработки нажмите <OK> ", "");
+		//MessageBox::Show("Для начала обработки нажмите <OK> ", "");
+		this->progressBar1->Visible = true;
+		double proBarVal = 0;
 		int val = 0;
 		for (int i = 0; i < x - 126; i++) {
 			val = 0;
 			for (int j = 0; j < 127; j++) {
 				val += data_result1[j] * dataF[i + j];
 			}
+
 			dataPROC[i] = val / 32767;
+			if ( i == proBarVal ){
+				this->progressBar1->Increment(1);
+				proBarVal += (x - 126) / 100;
+			}
 
 		}
 
 		this->labelStat->Text = "Данные обработаны";
-		
+		this->progressBar1->Increment(-100);
+		this->progressBar1->Visible = false;
 		
 
 		
